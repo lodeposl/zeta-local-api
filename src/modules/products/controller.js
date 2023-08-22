b4806e3040c6937b55611a761b8bbde683b29f6c
joshua oamaña <joshuacher16@gmail.com>
2023-08-22 12:36:42 -0400

    logo

--- src/modules/products/controller.js
+++ src/modules/products/controller.js
@@ -2,6 +2,7 @@ import qrcode from "qrcode"
 import sql from "../../utils/mssql.js"
 import fs from "fs"
 import { PRODUCT_BY_CODE, FIRM_AND_COUNT } from "./queries.js"
+import    { createCanvas, loadImage } from 'canvas';
 const controller = {
     queryMarcas: async(body, params)=>{
         let error
@@ -59,6 +60,27 @@ const controller = {
                     errorCorrectionLevel:"M"
                 })
             }
+
+            const canvas = createCanvas(180, 180);
+            await qrcode.toCanvas(canvas, url, {
+                version:5,
+                errorCorrectionLevel:"H"
+            })
+            const imageObj = await loadImage("./public/logo-qr.png")
+            imageObj.style="background-color:red;"
+            
+            const imgDim = { width: 60, height: 20 }; // dimensions of the logo
+            const context = canvas.getContext('2d');
+            context.drawImage(
+              imageObj,
+              canvas.width / 2 - imgDim.width / 2,
+              canvas.height / 2 - imgDim.height / 2,
+              imgDim.width,
+              imgDim.height
+            );
+            
+            fs.writeFileSync("./public/xd.png", canvas.toBuffer())
+            
 
 
 
