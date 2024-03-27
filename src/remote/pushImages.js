5b9b01d1780612fd2af2eeb2b43bc2aa1bb0385a
joshua oamaña <joshuacher16@gmail.com>
2024-03-27 11:38:55 -0400

    Image compression

--- src/remote/pushImages.js
+++ src/remote/pushImages.js
@@ -5,8 +5,10 @@ import { Image } from "../models/images.js"
 import { firebase } from "../firebase.js"
 import {getStorage} from "firebase-admin/storage"
 import fs from "fs"
-
+import crypto from "crypto"
+import path from "path"
 import { initMongo } from "../utils/mongo.js"
+import sharp from "sharp"
 async function task (){
     try{
         await initMongo()
@@ -28,10 +30,20 @@ async function task (){
         //inicializar imagenes
         const firebaseBucket = getStorage()
 
+        //Comprimir imagenes
+        const files = fs.readdirSync("./public/productos")
+        for( const file of files){
+            const img = await sharp(path.join("./public/productos", file))
+            .resize({height:500, width:500,fit:"inside"})
+            .jpeg({ quality: 80, progressive: true, mozjpeg: true })
+            .toFile(path.join("./public/compressed",(file.split(".")[0])+".jpeg"))
+        }
+        //Comprimir imagenes
+
 
         //THIS JOB REQUIRES STILL:
-        //  -Testing images Hash´s to not re-upload the same image
-        //  -Compressing images to a standard size
+        // DONE -Testing images Hash´s to not re-upload the same image
+        // DONE -Compressing images to a standard size
         //  -Maybe make it so that only few at a time are updated
 
         const config = await Config.findOne()
@@ -39,34 +51,52 @@ async function task (){
         if (imagesNotUpdated.length==0){
             config.imageUpdate+=1
             await config.save()
-        }else{
+            console.log("config updated")
+        }
             for (const image of imagesNotUpdated){
                 try{
-                    await new Promise((resolve,reject)=>{
-                        fs.readFile("./public/productos/"+image.ItemCode+".png", (err,data)=>{
+                    const imageData = await Image.findOne({ItemCode:image.ItemCode})
+                    const update = await new Promise((resolve,reject)=>{
+                        fs.readFile("./public/compressed/"+image.ItemCode+".jpeg", (err,data)=>{
+
                             if (!err && data){
-                                const options = {
-                                    destination: `products/${image.ItemCode}.png`,
-                                }     
-                                    firebaseBucket.bucket().upload("./public/productos/"+image.ItemCode+".png", options).then((res)=>{
-                                        resolve()
+                                const hashSum = crypto.createHash('sha256');
+                                hashSum.update(data);
+                                const digest = hashSum.digest("base64")
+                                if (imageData.hash != digest ){
+                                    console.log("Its different, update it")
+                                    const options = {
+                                        destination: `products/${image.ItemCode}.jpeg`,
+                                    }     
+                                    firebaseBucket.bucket().upload("./public/compressed/"+image.ItemCode+".jpeg", options).then((res)=>{
+                                        resolve(digest)
                                     }).catch((err)=>{
                                         reject(err)
                                     })
+                                }else{
+                                    console.log("Its the same, ignore it")
+                                    resolve(false)
+                                }
+
                             }else{
                                 //if no image exists, ignore it
-                                resolve()
+                                resolve(false)
                             }
                         })
                     })
-                    await Image.updateOne({ItemCode:image.ItemCode}, {$set:{lastUpdate:config.imageUpdate}})
+                    if (update){
+                        imageData.hash = update
+                    }
+                    imageData.lastUpdate = config.imageUpdate
+                    await imageData.save()
+                    // await Image.updateOne({ItemCode:image.ItemCode}, {$set:{lastUpdate:config.imageUpdate}})
 
                 }catch(error){
                     console.log("error", error)
                 }
             }
             console.log("All images updated")
-        }
+        
 
 
     }catch(error){
