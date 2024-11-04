3583e88b7a97f8d838144fda764761383e1b8ae8
Joshua Omaña <joshuacher16@gmail.com>
2024-11-04 12:51:26 -0400

    new ip because fuck this

--- src/modules/products/controller.js
+++ src/modules/products/controller.js
@@ -162,7 +162,7 @@ const controller = {
         let x
         let e
         try {
-            const api = axios.create( {baseURL: "http://192.168.0.105:4000"})
+            const api = axios.create( {baseURL: "http://192.168.0.109:4000"})
             const r = await api.post("products/jspdf", {
                 products:body.products,
                 props:body.props
